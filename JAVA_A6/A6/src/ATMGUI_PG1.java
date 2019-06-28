bd117e63aef5283e292b0b5a2b876f0a98e85627
unknown <IvyLeano@outlook.com>
2019-06-27 22:23:11 -0400

    second commit

--- JAVA_A6/A6/src/ATMGUI_PG1.java
+++ JAVA_A6/A6/src/ATMGUI_PG1.java
@@ -8,38 +8,46 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.util.Scanner;
 
-public class ATMGUI extends Application {
-	@Override
+public class ATMGUI_PG1 extends Application {
+	// a temporary variable to store user pin input
+	static int tempPin = 0;
+
+	// ATM pin prompting screen
 	public void start(Stage stage) {
 
-		// Creating a grid pane 
+		// Creating a grid pane
 		GridPane pane = new GridPane();
 
 		pane.setPadding(new Insets(15, 15, 15, 15));
 		pane.setHgap(8);
 		pane.setVgap(20);
 
+		TextField userInput = new TextField();
+
 		// Account Number node
-		pane.add(new Label("Enter an Account number: "), 0,0);
-		pane.add(new TextField(), 1, 0);
+		pane.add(new Label("Enter an Account number: "), 0, 0);
+		pane.add(userInput, 1, 0);
 
 		// Submit Button
 		Button btAdd = new Button("Submit");
-		//Scanner input = new Scanner(System.in);
-		//int accountNumber = input.nextInt();
-		//input.close();
 		pane.add(btAdd, 1, 1);
-	
+
+		// on submit button click, call onClick()
+		btAdd.setOnAction(action -> onClick(userInput));
+
 		Scene scene = new Scene(pane);
 
 		stage.setTitle("ATM"); // Set the stage title
 		stage.setScene(scene); // Place the scene in the stage
 		stage.show(); // Display the stage
-		//System.out.print(accountNumber);
-		//return accountNumber;
 	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+	//sets the t
+	public void onClick(TextField userInput) {
+			try {
+				tempPin = Integer.parseInt(userInput.getText());
+				//if(tempPin > 4 || tempPin < 4) { throw new Exception();}
+			} catch (Exception e) {
+				System.out.print("Invalid Pin: Please enter a 4 digit pin\n");
+			}
+		}
 }
---
+++ JAVA_A6/A6/src/ATMGUI_PG2.java
@@ -0,0 +1,59 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+//import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+//import java.util.Scanner;
+
+public class ATMGUI_PG2 extends Application {
+
+	// ATM welcome screen
+	public void start(Stage stage) {
+
+		// Creating a grid pane
+		GridPane pane = new GridPane();
+
+		pane.setPadding(new Insets(30, 100, 30, 100));
+		pane.setHgap(8);
+		pane.setVgap(8);
+
+		pane.add(new Label("   Welcome Account 1234"), 0, 0); // change this
+		pane.add(new Label("What would you like to do?"), 0, 1); // change this
+
+		// Check Balance Button
+		Button btBalance = new Button("Check Balance");
+		pane.add(btBalance, 0, 2);
+		btBalance.setMaxWidth(Double.MAX_VALUE);
+
+		// Check Withdraw Button
+		Button btWithdraw = new Button("Withdraw Money");
+		pane.add(btWithdraw, 0, 3);
+		btWithdraw.setMaxWidth(Double.MAX_VALUE);
+		
+		// Check Balance Button
+		Button btDeposit = new Button("Deposit Money");
+		pane.add(btDeposit, 0, 4);
+		btDeposit.setMaxWidth(Double.MAX_VALUE);
+		
+		// Check Balance Button
+		Button btExit = new Button("Exit the Account");
+		pane.add(btExit, 0, 5);
+		btExit.setMaxWidth(Double.MAX_VALUE);
+
+		// on submit button click, call onClick()
+		// btAdd.setOnAction(action -> onClick(userInput));
+
+		Scene scene = new Scene(pane);
+
+		stage.setTitle("ATM"); // Set the stage title
+		stage.setScene(scene); // Place the scene in the stage
+		stage.show(); // Display the stage
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
