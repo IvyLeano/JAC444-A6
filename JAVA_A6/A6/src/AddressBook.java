7e5bdbb25143d2a8c6c10b36711c5cb039298217
unknown <IvyLeano@outlook.com>
2019-06-30 21:59:19 -0400

    task 2 done

--- JAVA_A6/A6/src/AddressBook.java
+++ JAVA_A6/A6/src/AddressBook.java
@@ -10,18 +10,29 @@ import javafx.stage.Stage;
 import javafx.scene.layout.ColumnConstraints;;
 
 public class AddressBook extends Application {
-
+	//Text field values and comboBox values
 	TextField fstn = new TextField();
 	TextField lstn = new TextField();
 	TextField city = new TextField();
 	TextField postcde = new TextField();
+	ComboBox<String> myComboBox = new ComboBox<String>();
 	
-	static int currentIndex = 0;
+	//temp user input values
+	String fn;
+	String ln;
+	String cty;
+	String prv;
+	String pstcde;
+	
+	//contactNo is the current contact being viewed in the window
+	//totalContacts is the total amount added so far
+	static int contactNo = 0;
+	static int totalContacts = 0;
 
 	AddressBookLogic addressBookLogic = new AddressBookLogic();
-
+	
 	public void start(Stage stage) {
-
+		addressBookLogic.clearFile(); //clears file prior to adding any new data
 		// Creating a grid pane with six columns of equal width (70)
 		GridPane pane = new GridPane();
 		pane.getColumnConstraints().add(new ColumnConstraints(70));
@@ -40,17 +51,15 @@ public class AddressBook extends Application {
 		pane.add(fstn, 1, 0, 5, 1);
 
 		// Last Name node
-	
 		pane.add(new Label("Last Name:"), 0, 1);
 		pane.add(lstn, 1, 1, 5, 1);
 
 		// City node
-		
 		pane.add(new Label("      City: "), 0, 2);
 		pane.add(city, 1, 2);
 
 		// Combo drop down menu for provinces
-		ComboBox<String> myComboBox = new ComboBox<String>();
+		myComboBox = new ComboBox<String>();
 		myComboBox.getItems().addAll("AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC", "SK", "YT");
 
 		// Province node
@@ -58,7 +67,6 @@ public class AddressBook extends Application {
 		pane.add(myComboBox, 3, 2);
 
 		// Postal Code node
-		
 		pane.add(new Label("Postal Code:"), 4, 2);
 		pane.add(postcde, 5, 2);
 
@@ -67,15 +75,23 @@ public class AddressBook extends Application {
 		pane.add(btAdd, 0, 3);
 		btAdd.setMaxWidth(Double.MAX_VALUE);
 
-		btAdd.setOnAction(action -> {
-			String fn = fstn.getText();
-			String ln = lstn.getText();
-			String cty = city.getText();
-			String prv = myComboBox.getValue();
-			String pstcde = postcde.getText();
+		btAdd.setOnAction(action -> { 
+			fn = fstn.getText(); //set temp values equal to text field user input
+			ln = lstn.getText();
+			cty = city.getText();
+			prv = myComboBox.getValue();
+			pstcde = postcde.getText();
 			
-			currentIndex = currentIndex + 1;
-			addressBookLogic.add(fn, ln, cty, prv, pstcde);
+			if(!addressBookLogic.validateInput(fn, ln, cty, prv, pstcde)) { //validate data 
+				System.out.print("Invalid input detected: please verify that all fields are valid. \n");
+				System.out.print("Fields cannot exceed 20 characters.\n");
+			}
+			else { //if valid data, add to file
+			addressBookLogic.write(fn, ln, cty, prv, pstcde);
+			contactNo = contactNo + 1; //iterate your contactNo (what you see in the window)
+			totalContacts = totalContacts + 1; //iterate total contacts
+			clearContactForm(); //clear form to prompt new input
+			}	
 		});
 
 		// First Button
@@ -83,7 +99,7 @@ public class AddressBook extends Application {
 		pane.add(btFirst, 1, 3);
 		btFirst.setMaxWidth(Double.MAX_VALUE);
 		btFirst.setOnAction(action -> {
-			retrieveContact("First");
+			buttonActionHandlers("First"); //calls function below
 		});
 
 		// Next Button
@@ -91,7 +107,9 @@ public class AddressBook extends Application {
 		pane.add(btNext, 2, 3);
 		btNext.setMaxWidth(Double.MAX_VALUE);
 		btNext.setOnAction(action -> {
-			retrieveContact("Next");
+			if(contactNo != totalContacts) { // if your current view is not the last possible view (because there's no next if you are at the end)
+				buttonActionHandlers("Next"); //calls function below
+				}
 		});
 
 		// Previous Button
@@ -99,7 +117,9 @@ public class AddressBook extends Application {
 		pane.add(btPrevious, 3, 3);
 		btPrevious.setMaxWidth(Double.MAX_VALUE);
 		btPrevious.setOnAction(action -> {
-			retrieveContact("Previous");
+			if(contactNo != 1) { //if  you are not at the first contact (there's nothing before the first)
+			buttonActionHandlers("Previous"); //calls function below
+			}
 		});
 
 		// Last Button
@@ -107,49 +127,69 @@ public class AddressBook extends Application {
 		pane.add(btLast, 4, 3);
 		btLast.setMaxWidth(Double.MAX_VALUE);
 		btLast.setOnAction(action -> {
-			retrieveContact("Last");
+			buttonActionHandlers("Last"); //calls function below
 		});
 
 		// Update Button
 		Button btUpdate = new Button("Update");
 		pane.add(btUpdate, 5, 3);
 		btUpdate.setMaxWidth(Double.MAX_VALUE);
+		btUpdate.setOnAction(action -> { 
+			fn = fstn.getText(); //set temp values equal to text field user input
+			ln = lstn.getText();
+			cty = city.getText();
+			prv = myComboBox.getValue();
+			pstcde = postcde.getText();
+			//validate data, trim() needed to remove buffered spaces from reading file
+			if(!addressBookLogic.validateInput(fn.trim(), ln.trim(), cty.trim(), prv.trim(), pstcde.trim())) { 
+				System.out.print("Invalid input detected: please verify that all fields are valid. \n");
+				System.out.print("Fields cannot exceed 20 characters.\n");
+			}
+			else {
+			addressBookLogic.update(contactNo, fn, ln, cty, prv, pstcde); // call function below
+			}});
 
 		Scene scene = new Scene(pane);
 
 		stage.setTitle("Address Book"); // Set the stage title
 		stage.setScene(scene); // Place the scene in the stage
 		stage.show(); // Display the stage
-		// addressBookLogic.test();////////////////////
-
 	}
-
-	void retrieveContact(String action) {
-		int index = currentIndex;
+	//clears the contact form upon adding a new contact
+	//allows for new user input
+    void clearContactForm() {
+    	fstn.setText("\0");
+		lstn.setText("\0");
+		city.setText("\0");
+		myComboBox.setValue("\0");
+		postcde.setText("\0");
+    }
+    //Event handlers, moves your position in the data and sets the text field to display that data
+	void buttonActionHandlers(String action) {
+		int index = contactNo;
 		switch (action) {
 		case "First":
-			index = 0;
+			index = 1; // sets position to 1st
 			break;
 		case "Next":
-			index = index + 1;
+			index = index + 1; //sets position to the one after current
 			break;
 		case "Previous":
-			index = index - 1;
+			index = index - 1; // sets position the one before current
 			break;
 		case "Last":
-			index = addressBookLogic.getFirstEmptyContactSlot() - 1;
+			index = totalContacts; // set position to total number of contacts or last
 		}
-		fstn.setText(addressBookLogic.getContact(index).getFirstName());
-		lstn.setText(addressBookLogic.getContact(index).getLastName());
-		city.setText(addressBookLogic.getContact(index).getCity());
-		//prv = addressBookLogic.getContact(index).getProvince();
-		postcde.setText(addressBookLogic.getContact(index).getPostalCode());
-		currentIndex = index;
-		// System.out.print("!!!!" + index);
-
+		fstn.setText(addressBookLogic.getField(index, "FN"));
+		lstn.setText(addressBookLogic.getField(index, "LN"));
+		city.setText(addressBookLogic.getField(index, "CTY"));
+		myComboBox.setValue(addressBookLogic.getField(index, "PRV"));
+		postcde.setText(addressBookLogic.getField(index, "PSTCDE"));
+		
+		contactNo = index; // changes the position you are at
 	}
-
+/*
 	public static void main(String[] args) {
 		launch(args);
-	}
+	}*/
 }
--- JAVA_A6/A6/src/AddressBookLogic.java
+++ JAVA_A6/A6/src/AddressBookLogic.java
@@ -1,113 +1,205 @@
-
-//A class that stores contact information
-class Contacts {
-	private String m_firstName;
-	private String m_lastName;
-	private String m_city;
-	private String m_province;
-	private String m_postalCode;
-
-	// No-argument default constructor
-	Contacts() {
-		m_firstName = "\0";
-		m_lastName = "\0";
-		m_city = "\0";
-		m_province = "\0";
-		m_postalCode = "\0";
-	}
-
-	// Setter methods for the Contact class
-	void setName(String fn, String ln) {
-		m_firstName = fn;
-		m_lastName = ln;
-	}
-
-	void setCity(String cty) {
-		m_city = cty;
-	}
-
-	void setProvince(String prv) {
-		m_province = prv;
-	}
-	void setPostalCode(String pstcde) {
-		m_postalCode = pstcde;
-	}
-
-	String getFirstName() {
-		return m_firstName;
-	}
-
-	String getLastName() {
-		return m_lastName;
-	}
-
-	String getCity() {
-		return m_city;
-	}
-
-	String getProvince() {
-		return m_province;
-	}
-
-	String getPostalCode() {
-		return m_postalCode;
-	}
-
-	// method used to check if there is no contact data stored
-	boolean isEmpty() {
-		return getFirstName() == "\0";
-	}
-}
+import java.io.RandomAccessFile;
 
 public class AddressBookLogic {
-	// The address book only has an array of contacts, set to 10 in this case
-	Contacts[] m_contacts = new Contacts[10];
-	int m_noContacts = 0;
 
-	AddressBookLogic() {
-		for (int i = 0; i < 10; i++) {
-			m_contacts[i] = new Contacts();
+	RandomAccessFile file;
+	String filename = "myFile.txt";
+
+	// pos indicates the position to write to in the file
+	// contactNo and contacts, are used to track/identify
+	// the number of contacts in the file
+	int pos = 2;
+	int contactNo = 1;
+	String contacts = "Contact Number: " + Integer.valueOf(contactNo) + ")";
+
+	// this function fixes the string length to 20, by adding spaces to user inputs less than 20
+	String addBuffer(String field) {
+		int buffer = 20 - field.length();
+		String tempfield = field;
+
+		for (int i = 0; i < buffer; i++) {
+			tempfield = tempfield + " ";
+		}
+		return tempfield;
+	}
+	// this function clears the contents of the file upon application execution
+	void clearFile() {
+		try {
+			file = new RandomAccessFile(filename, "rw");
+			file.setLength(0);
+		} catch (Exception e) {
 		}
 	}
 
-	int getFirstEmptyContactSlot() {
-		int index = 0;
-		check : for(int i = 0; i < 10; i++) {
-			if(m_contacts[i].isEmpty()) { 
-				index = i;
-				break check;
+	// Writes to the file, when add button is clicked
+	void write(String fn, String ln, String cty, String prv, String pstcode) {
+		
+		try {
+			file = new RandomAccessFile(filename, "rw");
+			String newline = "\r\n";
+			
+			// write contact heading to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(contacts).getBytes());
+			contactNo = contactNo + 1;
+			contacts = "Contact Number: " + Integer.valueOf(contactNo) + ")";
+			pos = pos + 21;
+			
+			// write first name to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(fn).getBytes());
+			pos = pos + 21;
+
+			// write last name to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(ln).getBytes());
+			pos = pos + 21;
+
+			// write city to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(cty).getBytes());
+			pos = pos + 21;
+
+			// write province to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(prv).getBytes());
+			pos = pos + 21;
+
+			// write postal code to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(pstcode).getBytes());
+			
+			file.write(newline.getBytes());
+			pos = pos + 24;
+			file.close();
+			
+		} catch (Exception e) {
+			System.out.print(e + "\n");
+		}
+		
+	}
+
+	// Read function returns a contacts information as a single string
+	// the line number is passed through, representing the contact number
+	String readFileLine(int line) {
+		
+		String contact = "\0";
+		int l = 1; //since contact number starts at 1
+		
+		try {
+			RandomAccessFile file = new RandomAccessFile(filename, "r");
+			again: while ((contact = file.readLine()) != null) { //while not at end of file, store line to "contact" variable
+				if (l == line) { //if the line number of interest (what's passed through) == the line counter "l" then break while
+					break again;
+				} else { //else continue until line of interest is reached and stored
+					l++;
 				}
+			}
+			file.close();
+		} catch (Exception e) {
+			System.out.print(e + "\n");
 		}
-		System.out.print("getFirstEmptyContactSlot(): " + index + "\n");
-		return index;
-	}
-	
-	// add button functionality, adds a contact
-	void add(String fn, String ln, String cty, String prv, String pstcode) {
-		int index = getFirstEmptyContactSlot();
-		m_contacts[index].setName(fn, ln);
-		m_contacts[index].setCity(cty);
-		m_contacts[index].setProvince(prv);
-		m_contacts[index].setPostalCode(pstcode);
-		
-		m_noContacts = m_noContacts + 1;
-		
-		System.out.print("getFirstEmptyContactSlot(): \n");
-	}
-	Contacts getContact(int index) {
-		return m_contacts[index];
-	}
-	///remove this when done
-	void test() {
-		 for(int i = 0; i < 10; i++) {
-			 System.out.print("test(): \n");
-			 System.out.print(m_contacts[i].getFirstName() + " " +
-			 m_contacts[i].getLastName()  + " " +
-			 m_contacts[i].getCity()  + " " +
-			 m_contacts[i].getProvince()  + " " +
-			 m_contacts[i].getPostalCode()  + "\n");
-			 
-		}
+
+		return contact; //return contact as a single string
 	}
 
+	//returns the field of interest, ie: first name, last name...
+	//based on the contact number and the field
+	String getField(int contactNo, String field) {
+		String contact = readFileLine(contactNo); //call the readFileLine() function to return data for "contactNo"
+
+		String fieldOfInterest = "\0"; 
+		switch (field) { //if field is "FN", "LN" ... return contact substring - ie: return first name, last name or city...
+		case "FN": 
+			fieldOfInterest = contact.substring(23, 43);
+			break;
+		case "LN":
+			fieldOfInterest = contact.substring(44, 63);
+			break;
+		case "CTY":
+			fieldOfInterest = contact.substring(65, 83);
+			break;
+		case "PRV":
+			fieldOfInterest = contact.substring(86, 103);
+			break;
+		case "PSTCDE":
+			fieldOfInterest = contact.substring(107, contact.length());
+
+		}
+		return fieldOfInterest;
+	}
+
+	//Updates based on contact number
+	void update(int contactNo, String fn, String ln, String cty, String prv, String pstcode) {
+
+		int fieldLen = 129;
+		contacts = "Contact Number: " + Integer.valueOf(contactNo) + ")";
+
+		try {
+			file = new RandomAccessFile(filename, "rw");
+			int pos = (2 + (fieldLen * (contactNo - 1))); //position starts at 2 + the length of the entire row
+														  // if contact number is 1, pos would just start at 2, otherwise multiply to get the number of pos before starting pos
+			file.seek(pos);
+			file.write(addBuffer(contacts).getBytes());
+			contactNo = contactNo + 1;
+			contacts = "Contact Number: " + Integer.valueOf(contactNo) + ")";
+			pos = pos + 21;
+
+			file.seek(pos);
+			file.write(addBuffer(fn).getBytes());
+			pos = pos + 21;
+
+			// write last name to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(ln).getBytes());
+			pos = pos + 21;
+
+			// write city to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(cty).getBytes());
+			pos = pos + 21;
+
+			// write province to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(prv).getBytes());
+			pos = pos + 21;
+
+			// write postal code to file, set newline and new position
+			file.seek(pos);
+			file.write(addBuffer(pstcode).getBytes());
+			pos = pos + 24;
+			file.write(("\r\n").getBytes());
+
+			file.close();
+		} catch (Exception e) {
+			System.out.print(e + "\n");
+		}
+	}
+	//validates postal code: every even index should be a letter, and every odd index a number
+	boolean validPostalCode(String pstcde) {
+		boolean valid = true;
+		check: for (int i = 0; i < pstcde.length(); i++) {
+			if (i % 2 != 0 && Character.isLetter(Character.toLowerCase(pstcde.charAt(i)))) { //odd
+				valid = false;
+				break check;
+			}
+			if (i % 2 == 0 && Character.isDigit(Character.toLowerCase(pstcde.charAt(i)))) { //even
+				valid = false;
+				break check;
+			}
+		}
+		return valid;
+	}
+	// validates user input
+	// I didn't validate repeat inputs since there was no primary key here to
+	// separate address info
+	boolean validateInput(String fn, String ln, String cty, String prv, String pstcde) {
+		boolean empty = fn.length() == 0 || ln.length() == 0 || cty.length() == 0 || pstcde.length() == 0 || prv == null;
+		boolean size = fn.length() > 20 || ln.length() > 20 || cty.length() > 20 || pstcde.length() > 20;
+		boolean alpha = fn.chars().allMatch(Character::isLetter) || ln.chars().allMatch(Character::isLetter)
+				|| cty.chars().allMatch(Character::isLetter);
+		boolean validPostal = pstcde.length() == 6 && validPostalCode(pstcde); // no space in postal code
+	
+		return validPostal && !empty && alpha && !size;
+	}
 }
